7d86a45bb8b1b8bddc8ce582ebb67cf9ac7351ab
Petr Jindra <petr@jindrovi.uno>
2023-01-05 20:54:05 +0100

    the Instance documentation

--- src/instance_management/instance.rs
+++ src/instance_management/instance.rs
@@ -2,27 +2,44 @@ use std::sync::Arc;
 
 use crate::types::InstanceManager;
 
+
+/// Always deliver same instance wrapped in the `Arc`
+///
+/// The instance is passed directly to the `new` method (no factory is used)
+///
+/// ## Typical use cases
+///
+/// * constants
+/// * configuration values
 pub struct Instance<T> {
-    value: Arc<T>,
+    /// Stored instance of T wrapped in the Arc
+    instance: Arc<T>,
 }
 
 
 impl<T> Instance<T> {
-    pub fn new(value: T) -> Self {
-        Self { value: Arc::new(value) }
+    /// Create new instance of the `Instance` with given value
+    pub fn new(instance: T) -> Self {
+        Self { instance: Arc::new(instance) }
     }
 }
 
 
 impl<T> InstanceManager<Arc<T>> for Instance<T> {
     fn get_instance(&self, _: &()) -> Arc<T> {
-        self.value.clone()
+        self.instance.clone()
     }
 }
 
 
 #[cfg(test)]
 mod tests {
+    use crate::instance_management::instance::Instance;
+    use crate::InstanceManager;
+
     #[test]
-    fn test_hovno() {}
+    fn test_initialization() {
+        let instance = Instance::new(42);
+        assert_eq!(*instance.get_instance(&()), 42);
+    }
 }
