010dc4d6a52a6380bba5499e5171884ce6c4ca96
Petr Jindra <petr@jindrovi.uno>
2023-01-10 12:33:57 +0100

    tests for singleton

--- src/instance_management/singleton.rs
+++ src/instance_management/singleton.rs
@@ -1,5 +1,4 @@
 use std::cell::RefCell;
-use std::ops::Deref;
 use std::sync::Arc;
 use crate::{InstanceManager};
 
@@ -16,6 +15,14 @@ impl<T> Singleton<T> {
         Self{instance: Arc::new(RefCell::new(None)), factory}
     }
 
+    pub fn is_empty(&self) -> bool {
+        self.instance.borrow().is_none()
+    }
+
+    pub fn clear(&mut self) {
+        *self.instance.borrow_mut() = None;
+    }
+
     fn prepare_instance(&self) {
         let mut instance = self.instance.borrow_mut();
         if instance.is_none() {
@@ -30,8 +37,58 @@ impl<T> Singleton<T> {
 
 
 impl<T> InstanceManager<Arc<T>> for Singleton<T> {
-    fn get_instance(&self, args: &()) -> Arc<T> {
+    fn get_instance(&self, _: &()) -> Arc<T> {
         self.prepare_instance();
         self.instance.as_ref().borrow().as_ref().unwrap().clone()
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use std::ptr::addr_of;
+    use crate::instance_management::singleton::Singleton;
+    use crate::InstanceManager;
+
+    fn make_singleton() -> Singleton<()> {
+        Singleton::new(Box::new(move |_| ()))
+    }
+
+    #[test]
+    fn is_empty_on_init() {
+        let s = make_singleton();
+        assert!(s.is_empty())
+    }
+
+    #[test]
+    fn is_not_empty_after_get_instance() {
+        let s = make_singleton();
+        s.get_instance(&());
+        assert!(!s.is_empty());
+    }
+
+    #[test]
+    fn return_same_instances() {
+        let s = make_singleton();
+        let v1 = s.get_instance(&());
+        let v2 = s.get_instance(&());
+
+        assert_eq!(*v1, *v2);
+    }
+
+    #[test]
+    fn clear_remove_value() {
+        let mut s = make_singleton();
+        s.clear();
+        assert!(s.is_empty());
+    }
+
+    #[test]
+    fn new_instance_created_after_clear() {
+        let mut s = make_singleton();
+        let v1 = s.get_instance(&());
+        s.clear();
+        let v2 = s.get_instance(&());
+        assert_ne!(addr_of!(*v1), addr_of!(*v2));
     }
 }
