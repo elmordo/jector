3da25bb0b224fe5108af994cf2bea9116508a6c0
Petr Jindra <petr@jindrovi.uno>
2023-01-11 17:17:38 +0100

    the prototype instance manager

--- src/instance_management/prototype.rs
+++ src/instance_management/prototype.rs
@@ -1,3 +1,5 @@
+use std::borrow::Borrow;
+use std::ops::Deref;
 use std::sync::Arc;
 use crate::InstanceManager;
 
@@ -6,14 +8,47 @@ pub struct Prototype<T> where T: Clone {
 }
 
 impl<T> Prototype<T> where T: Clone {
-    pub fn new(instance: T) {
-        Prototype::new(Arc::new(instance))
+    pub fn new(instance: T) -> Prototype<T> {
+        Prototype{instance: Arc::new(instance)}
     }
 }
 
 
 impl<T> InstanceManager<T, ()> for Prototype<T> where T: Clone {
     fn get_instance(&self, args: &()) -> T {
-        self.instance.clone()
+        self.instance.deref().clone()
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use std::cell::RefCell;
+    use crate::instance_management::prototype::Prototype;
+    use crate::InstanceManager;
+
+    struct MyStruct {
+        val: i32,
+        offset: RefCell<i32>
+    }
+
+    impl Clone for MyStruct {
+        fn clone(&self) -> Self {
+            *self.offset.borrow_mut() += 1;
+            MyStruct{val: self.val + *self.offset.borrow(), offset: RefCell::new(0)}
+        }
+    }
+
+    fn make_prototype() -> Prototype<MyStruct> {
+        Prototype::new(MyStruct{val: 42, offset: RefCell::new(0)})
+    }
+
+    #[test]
+    fn copy_value() {
+        let p = make_prototype();
+        let v1 = p.get_instance(&());
+        let v2 = p.get_instance(&());
+
+        assert_ne!(v1.val, v2.val);
     }
 }
